c79b7bc789ab1c4b8a8f3e563ab949f78b39e9a0
Scott <scottherlihy1@gmail.com>
2018-08-13 10:11:40 -0500

    not sure what's in this diff`

--- basketball/Team/Team.py
+++ basketball/Team/Team.py
@@ -91,53 +91,7 @@ class Team(object):
         print '---------------------------------' + '\n'
 
 
-class DefaultBasketballTeam(Team):
-    def __init__(self, players): #Check if the players are of a type that's not supported? else case in Team?
-            constraints = {PointGuard : Allowance(1, 3),
-                       ShootingGuard: Allowance(1, 3),
-                       PowerForward: Allowance(1, 3),
-                       SmallForward: Allowance(1, 3),
-                       Center: Allowance(1, 3),
-                       Guard: Allowance(3, 4),
-                       Forward: Allowance(3, 4),
-                       Utility: Allowance(8)}
-            super(DefaultBasketballTeam, self).__init__(players, constraints)
-
-
-#can positions be strings again? no because isinstance could maybe make tuples or something
-class Utility(object):
-    def __init__(self):
-        pass
-
-class Guard(Utility):
-    pass
-
-
-class Forward(Utility):
-    pass
-
-
-class PointGuard(Guard):
-    pass
-
-
-class ShootingGuard(Guard):
-    pass
-
-
-class PowerForward(Forward):
-    pass
-
-
-class SmallForward(Forward):
-    pass
-
-
-class Center(Utility):
-    pass
-
-
-from .GetPlayers import Player
+# from GetPlayers import Player
 pg =  Player(PointGuard(), 'name', 100)
 sg =  Player(ShootingGuard(), 'name1', 100)
 pf =  Player(PowerForward(), 'name2', 100)
